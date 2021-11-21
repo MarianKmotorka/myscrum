72d6e04eeacaaf5af91c661f2fc1217302cf143f
Marian Kmotorka <Marian.Kmotorka@quest.com>
2021-11-21 15:58:28 +0100

    styles

--- myscrum/ClientApp/src/pages/Home/utils.ts
+++ myscrum/ClientApp/src/pages/Home/utils.ts
@@ -4,12 +4,11 @@ export const projectCardProps = {
   border: 'solid 1px',
   borderColor: 'gray.200',
   padding: '15px',
-  _hover: { boxShadow: '0 0 20px rgba(0,0,0,0.1)', borderColor: 'primary' },
-  transition: '0.1s'
+  _hover: { boxShadow: '0 0 20px rgba(0,0,0,0.1)' },
+  transition: '0.2s'
 }
 
 export const selectedCardOverrideProps = {
-  border: 'solid 2px',
   borderColor: 'primary',
   color: 'primary',
   fontWeight: 500
